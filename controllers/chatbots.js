8e43c342ff9feb7b6df7d8c979d529517285332c
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-03-18 14:52:40 -0400

    Groq, OpenAI, Langchain, and LlamaIndex chatbots added

--- controllers/chatbots.js
+++ controllers/chatbots.js
@@ -1,49 +1,171 @@
 // -------------------- Packages --------------------
 var OpenAI = require("openai");
+var GroqSDK = require("groq-sdk");
+var { ChatGroq } = require("@langchain/groq");
+var { ChatPromptTemplate } = require("@langchain/core/prompts");
+var fs = require("node:fs/promises");
+var { Document, Groq, VectorStoreIndex, serviceContextFromDefaults } = require("llamaindex");
 
 
-// ----------------- OpenAI API Key -----------------
+// -------------------- API Keys --------------------
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1"
+});
+
+const groqSdk = new GroqSDK((
+    api_key = process.env.GROQ_API_KEY
+));
+
+const groq = new Groq((
+    api_key = process.env.GROQ_API_KEY
+));
+
+const model = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
 });
 
 
 // ---------------- Export Functions ----------------
 module.exports = {
     index,
-    new: newText
+    newOpenAI: newOpenAI,
+    newGroq: newGroq,
+    newLangchain: newLangchain,
+    newLlamaIndex: newLlamaIndex
 }
 
 
 // -------------------- Functions -------------------
-// Render the Chatbots index page
+// Render the Chatbots index page with empty AI chat responses
 function index(req, res) {
     res.render("chatbots/index", {
-        title: "Chatbot AI",
-        response: "",                                  // initialize the user response to an empty string
+        title: "AI Chatbots",
+        resGroq: "",
+        resOpenAI: "",                                                           
+        resLangchain: "",
+        resLlamaIndex: "",
         errorMsg: ""
     });
 }
 
-// Render the AI response from the OpenAI Chatbot
-async function newText(req, res) {
+// Render the AI response from Groq
+async function newGroq(req, res) {
     const userPrompt = req.body.inputText;             // get the user inputted text
 
-    // generate the response from the OpenAI API
-    var response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{
-            "role": "user",
-            "content": userPrompt                      // user inputted text to be generated from 
-        }],
-        max_tokens: 100                                // max number of characters to generate
+    // generate the response
+    const completion = await groqSdk.chat.completions
+    .create({
+      messages: [
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      model: "mixtral-8x7b-32768",
+    })
+    .then((chatCompletion) => {
+        // save the AI response
+        resGroq = chatCompletion.choices[0]?.message?.content || "";
+
+        // render the response on the Chatbots Index page
+        res.render("chatbots/index", {
+            title: "AI Chatbots",
+            resOpenAI: "",
+            resGroq,
+            resLangchain: "",
+            resLlamaIndex: "",
+            errorMsg: ""
+        });
     });
-    response = response.choices[0].message.content;    // get the generated response from the generated object
+}
+
+// Render the AI response from the OpenAI
+async function newOpenAI(req, res) {
+    const userPrompt = req.body.inputText;             // get the user inputted text
+
+    // generate the response
+    var resOpenAI = await openai.chat.completions.create({
+        messages: [{ 
+            role: "system", 
+            content: userPrompt 
+        }],
+        model: "mixtral-8x7b-32768",
+      });
+
+      // save the AI response
+    resOpenAI = resOpenAI.choices[0].message.content;
 
     // render the response on the Chatbots Index page
     res.render("chatbots/index", {
-        title: "Chatbot AI",
-        response,
+        title: "AI Chatbots",
+        resOpenAI,
+        resGroq: "",
+        resLangchain: "",
+        resLlamaIndex: "",
+        errorMsg: ""
+    });
+}
+
+// Render the AI response from Langchain
+async function newLangchain(req, res) {
+    const userPrompt = req.body.inputText;             // get the user inputted text
+
+    const prompt = ChatPromptTemplate.fromMessages([
+        ["system", "You are a helpful assistant"],
+        ["human", "{input}"],
+    ]);
+
+    const chain = prompt.pipe(model);
+
+    const response = await chain.invoke({
+        input: userPrompt,
+    });
+
+    // save the AI response
+    let resLangchain = response.content;
+    
+    // render the response on the Chatbots Index page
+    res.render("chatbots/index", {
+        title: "AI Chatbots",
+        resOpenAI: "",
+        resGroq: "",
+        resLangchain,
+        resLlamaIndex: "",
+        errorMsg: ""
+    });
+}
+
+// Render the AI response from LlamaIndex
+async function newLlamaIndex(req, res) {
+    const userPrompt = req.body.inputText;             // get the user inputted text
+
+    const serviceContext = serviceContextFromDefaults({ llm: groq });
+    const path = "node_modules/llamaindex/examples/abramov.txt";
+    const essay = await fs.readFile(path, "utf-8");
+    const document = new Document({ text: essay, id_: "essay" });
+    const index = await VectorStoreIndex.fromDocuments([document], {
+        serviceContext,
+    });
+    const retriever = index.asRetriever();
+    const queryEngine = index.asQueryEngine({
+        retriever,
+    });
+    const query = userPrompt;
+    const response = await queryEngine.query({
+        query,
+    });
+
+    // save the AI response
+    let resLlamaIndex = response;
+
+    // render the response on the Chatbots Index page
+    res.render("chatbots/index", {
+        title: "AI Chatbots",
+        resOpenAI: "",
+        resGroq: "",
+        resLangchain: "",
+        resLlamaIndex,
         errorMsg: ""
     });
 }
--- routes/chatbots.js
+++ routes/chatbots.js
@@ -15,8 +15,17 @@ var ensureLoggedIn = require('../config/ensureLoggedIn');
 // GET route for the chatbot's index page 
 router.get('/index', ensureLoggedIn, chatbotsCtrl.index);
 
-// POST route to generate a new AI chat from the prompt
-router.post("/new", ensureLoggedIn, chatbotsCtrl.new);  
+// POST route to generate a new OpenAI chat from the prompt
+router.post("/newOpenAI", ensureLoggedIn, chatbotsCtrl.newOpenAI);  
+
+// POST route to generate a new Groq AI chat from the prompt
+router.post("/newGroq", ensureLoggedIn, chatbotsCtrl.newGroq);  
+
+// POST route to generate a new Langchain AI chat from the prompt
+router.post("/newLangchain", ensureLoggedIn, chatbotsCtrl.newLangchain);  
+
+// POST route to generate a new Langchain AI chat from the prompt
+router.post("/newLlamaIndex", ensureLoggedIn, chatbotsCtrl.newLlamaIndex);  
 
 
 // ---------------- Export the router ---------------
