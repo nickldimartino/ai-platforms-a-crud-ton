68c1352f95a1cc46a2178e5f870d17fd4071c6ed
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-03-18 16:59:01 -0400

    Seeded AI Platform data

---
+++ data.js
@@ -0,0 +1,34 @@
+exports.platforms = [
+    { name: "ChatSimple", description: "For sales, support, lead generation, book meetings & more.", industry: "Business" },
+    { name: "HubSpot", description: "Connects data, teams, and customers on one customer platform.", industry: "Business" },
+    { name: "Lavender AI", description: "Helps sellers write better emails and get more replies in less time.", industry: "Business" },
+
+    { name: "Midjourney", description: "An independent research lab exploring new mediums of thought and expanding the imaginative powers of the human species.", industry: "Image" },
+    { name: "DALL-E 2", description: "Creates realistic images and art from a description in natural language.", industry: "Image" },
+    { name: "Adobe Firefly", description: "Uses generative AI and simple text prompts to create the highest-quality images, text effects, and color paletes.", industry: "Image" },
+
+    { name: "Pika", description: "An idea-to-video platform that sets a person's creativity in motion.", industry: "Video" },
+    { name: "Synthesia", description: "Creates studio-quality videos with AI avatars and voiceovers in 130+ languages.", industry: "Video" },
+    { name: "Hey Gen", description: "Creates videos in multiple languages with text-to-speech voiceovers.", industry: "Video" },
+    { name: "Invideo", description: "Takes any topic and generates a script, scenes, and voiceovers that can be tweaked.", industry: "Video" },
+
+    { name: "ChatGPT", description: "A free-to-use AI chatbot for engaging in conversations, gaining insights, and automating tasks.", industry: "AI Assistant" },
+    { name: "Character.ai", description: "Creating revolutionary open-ended conversational applications through breakthrough research.", industry: "AI Assistant" },
+    { name: "Wand", description: "A platform that enables businesses to develop solutions using a drag-and-drop interface.", industry: "AI Assistant" },
+
+    { name: "GitHub Copilot", description: "A pair programmer to autocomplete code and give developers suggestions.", industry: "Coding" },
+    { name: "AskCodi", description: "A tool too craft and amplify efficiency by creating smooth, manageable code and processes.", industry: "Coding" },
+    { name: "Tabnine", description: "Boost engineering velocity, code quality, and developer happiness by automating the coding workflow.", industry: "Coding" },
+    { name: "Replit", description: "Answers user questions to code-related problems.", industry: "Coding" },
+    { name: "Codeium", description: "Toolkit for developers with code suggestions.", industry: "Coding" },
+
+    { name: "Jasper", description: "Copilot for enterprise marketing teams who want better outcomes, not just faster outputs.", industry: "Writing" },
+    { name: "Wordtune", description: "An all-in-one GenAI platform to help you boost productivity and write with confidence.", industry: "Writing" },
+    { name: "Rytr", description: "The AI Writing Assistant helps your team produce more high-quality content in less time.", industry: "Writing" },
+
+    { name: "Notion", description: "It's the all-in-one workspace for you, your team, and your whole company.", industry: "Productivity" },
+    { name: "Zapier", description: "Get your software and data working together, automatically.", industry: "Productivity" },
+    { name: "Mem", description: "The self-organizing workspace.", industry: "Productivity" },
+
+    { name: "Apollo", description: "Finds ideal propects for sales.", industry: "Sales" },
+];
---
+++ seed.js
@@ -0,0 +1,42 @@
+// seed.js (a utility to seed/initialize the database)
+require('dotenv').config();
+require('./config/database');
+
+const Platform = require('./models/platform');
+const Company = require('./models/company');
+
+// For better organization, the seed data is being stored in a separate data.js module
+const data = require('./data');
+
+// // await needs an async function - use an async IIFE!
+// (async function() {
+//   let results = await Platform.deleteMany({});
+//   // results will be whatever the promise
+//   // returned by the deleteMany method resolves to
+//   console.log(results);
+  
+//   // Lastly, use process.exit() to "cleanly" shut down the Node program
+//   process.exit();
+// })();
+
+// removes documents from the movies and performers collections in paralllel
+(async function() {
+  // Save the promises (or call right in the array if feeling frisky)
+  const p1 = Platform.deleteMany({});
+  const p2 = Company.deleteMany({});
+  // Promise.all will return a single promise that resolves
+  // only after all of the array's promises resolve
+  let results = await Promise.all([p1, p2, p3]);
+  // results will be an array of result objects!
+  console.log(results);
+
+  // This time, provide the array of promises in-line
+  results = await Promise.all([
+    Platform.create(data.platforms),
+  ]);
+  console.log('Created platforms:', results[0]);
+
+  // Lastly, use process.exit() to "cleanly" shut down the Node program
+  process.exit();
+})();
+
